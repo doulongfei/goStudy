46df4e4001b41806985e67ce8ad12a2becb085fb
lfdou <2648142550@qq.com>
2022-12-11 00:13:19 +0800

    #使用shell脚本和jq工具库获取docker私有仓库的地址获取所有的镜像和tag信息，

--- src/ch4/dockerImages.sh
+++ src/ch4/dockerImages.sh
@@ -12,12 +12,22 @@ do
 #	echo $image
 	im=${image//\"}
 	im=${im//\,}
-	tags=$(curl -s ${url}/v2/${im}/tags/list | ./jq .tags)
+#	tags=$(curl -s http://192.168.94.66:5000/v2/$im/tags/list | ./jq .tags)
+#	echo $im
+	tags=$(curl -s ${url}/v2/${im}/tags/list | ./jq-linux64 .tags)
+#	if [[ -n ${tags} ]]; then
+#		tags=${tags:1:-1}
+#	else
+#		tags=noTag
+#	fi
 	tags=${tags:1:-1}
 	tag=${tags//\"}
 	tag=${tag//\,}
+#	echo $tag
+
 	for t in $tag
 	do
+#		echo $i
 		resultArr[$i]=$im:$t
 		i=`expr $i + 1`;
 	done
