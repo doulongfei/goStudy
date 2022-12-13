b57ef6d03ea33267b60a55619de14163c0d2f844
lfdou <2648142550@qq.com>
2022-12-14 00:36:53 +0800

    请实现一个函数，将一个字符串s中的每个空格替换成“%20”。 例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。 循环解法

--- src/offer/replaceSoace.go
+++ src/offer/replaceSoace.go
@@ -6,3 +6,17 @@ func replaceSpaceStr(s string) string {
 	var split []string = strings.Split(s, " ")
 	return strings.Join(split, "%20")
 }
+
+func replaceSpaceStr2(s string) string {
+
+	var str string
+	for i := 0; i < len(s); i++ {
+		if s[i] != ' ' {
+			str += s[i : i+1]
+		} else {
+			str += "%20"
+		}
+	}
+	return str
+
+}
