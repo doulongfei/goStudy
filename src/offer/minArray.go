39cdf1738d0b8fa203ec19ead9745831a5d7e291
lfdou <2648142550@qq.com>
2022-12-22 00:46:46 +0800

    BM19 寻找峰值

--- src/offer/minArray.go
+++ src/offer/minArray.go
@@ -47,3 +47,24 @@ func search(nums []int, target int) int {
 	}
 	return -1
 }
+
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ *
+ * @param nums int整型一维数组
+ * @return int整型
+ */
+func findPeakElement(nums []int) int {
+	// write code here
+	left, right := 0, len(nums)-1
+	for left < right {
+		mid := (left + right) >> 1
+		if nums[mid] > nums[mid+1] {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return right
+}
