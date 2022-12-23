ab9efc86551c8219aab122f5a5517af318287864
lfdou <2648142550@qq.com>
2022-12-23 23:11:44 +0800

    dfs

--- src/offer/dfs.go
+++ src/offer/dfs.go
@@ -1,3 +1,14 @@
 package offer
 
 // JZ12 矩阵中的路径
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ *
+ * @param matrix char字符型二维数组
+ * @param word string字符串
+ * @return bool布尔型
+ */
+func hasPath(matrix [][]byte, word string) bool {
+	// write code here
+}
