12374a9157a0d4b1065bb2ca9ebb4d00a63493cb
lfdou <2648142550@qq.com>
2022-12-28 00:22:06 +0800

    JZ18 删除链表的节点2

--- src/offer/deleteNode.go
+++ src/offer/deleteNode.go
@@ -4,13 +4,28 @@ func deleteNode(head *ListNode, val int) *ListNode {
 
 	res := &ListNode{}
 	res.Next = head
-	p := res
-	for cur := head; nil != cur.Next; cur = cur.Next {
-		if val == p.Next.Val {
-			p.Next = cur.Next
+
+	pre := res
+	cur := head
+
+	for cur != nil {
+		if cur.Val == val {
+			pre.Next = cur.Next
 			break
 		}
-		p = cur
+		pre = cur
+		cur = cur.Next
+
 	}
 	return res.Next
+
+	//p := res
+	//for cur := head; nil != cur.Next; cur = cur.Next {
+	//	if val == p.Next.Val {
+	//		p.Next = cur.Next
+	//		break
+	//	}
+	//	p = cur
+	//}
+	//return res.Next
 }
