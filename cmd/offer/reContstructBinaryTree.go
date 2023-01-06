9c05d8402fe7693f50f3dfdac48050347aabf02b
lfdou <2648142550@qq.com>
2023-01-06 23:29:11 +0800

    JZ26 树的子结构

--- cmd/offer/reContstructBinaryTree.go
+++ cmd/offer/reContstructBinaryTree.go
@@ -1,10 +1,10 @@
 package main
 
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
+//type TreeNode struct {
+//	Val   int
+//	Left  *TreeNode
+//	Right *TreeNode
+//}
 
 func reBinaaryTree(pre []int, vin []int) *TreeNode {
 	if len(pre) == 0 {
---
+++ cmd/offer/tree.go
@@ -0,0 +1,18 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ * @param pRoot1 TreeNode类
+ * @param pRoot2 TreeNode类
+ * @return bool布尔型
+ */
+func HasSubtree(pRoot1 *TreeNode, pRoot2 *TreeNode) bool {
+	// write code here
+}
