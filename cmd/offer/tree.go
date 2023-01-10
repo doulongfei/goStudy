15971eedec925ed2c0104aafbfa2643d3e13f1ea
lfdou <2648142550@qq.com>
2023-01-10 23:47:59 +0800

    JZ26 树的子结构 golang

--- cmd/offer/tree.go
+++ cmd/offer/tree.go
@@ -15,5 +15,31 @@ type TreeNode struct {
  */
 func HasSubtree(pRoot1 *TreeNode, pRoot2 *TreeNode) bool {
 	// write code here
-	return false
+
+	if pRoot2 == nil {
+		return false
+	}
+	if pRoot1 == nil && pRoot2 != nil {
+		return false
+	}
+	if pRoot1 == nil || pRoot2 == nil {
+		return true
+	}
+	flag1 := recursion(pRoot1, pRoot2)
+	flag2 := HasSubtree(pRoot1.Left, pRoot2)
+	flag3 := HasSubtree(pRoot1.Right, pRoot2)
+	return flag1 || flag2 || flag3
+}
+
+func recursion(root1 *TreeNode, root2 *TreeNode) bool {
+	if root1 == nil && root2 != nil {
+		return false
+	}
+	if root1 == nil || root2 == nil {
+		return true
+	}
+	if root1.Val != root2.Val {
+		return false
+	}
+	return recursion(root1.Left, root2.Left) && recursion(root1.Right, root2.Right)
 }
