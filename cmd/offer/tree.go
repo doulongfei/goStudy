a13151e6cf10a0240773abcdf92d839cbf13f27a
lfdou <2648142550@qq.com>
2023-01-15 23:06:05 +0800

    JZ27 二叉树的镜像

--- cmd/offer/tree.go
+++ cmd/offer/tree.go
@@ -43,3 +43,15 @@ func recursion(root1 *TreeNode, root2 *TreeNode) bool {
 	}
 	return recursion(root1.Left, root2.Left) && recursion(root1.Right, root2.Right)
 }
+
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ *
+ * @param pRoot TreeNode类
+ * @return TreeNode类
+ */
+func Mirror(pRoot *TreeNode) *TreeNode {
+	// write code here
+	return pRoot
+}
