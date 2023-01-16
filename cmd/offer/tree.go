d8b0f408c6aedc539cc3015ca78e69def039992a
lfdou <2648142550@qq.com>
2023-01-16 22:51:38 +0800

    JZ28 对称的二叉树

--- cmd/offer/tree.go
+++ cmd/offer/tree.go
@@ -51,7 +51,19 @@ func recursion(root1 *TreeNode, root2 *TreeNode) bool {
  * @param pRoot TreeNode类
  * @return TreeNode类
  */
+
 func Mirror(pRoot *TreeNode) *TreeNode {
 	// write code here
+
 	return pRoot
+}
+
+/*
+* 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+*
+* @param pRoot TreeNode类
+* @return bool布尔型
+ */
+func isSymmetrical(pRoot *TreeNode) bool {
+	// write code here
 }
