ab22182ea1119c286f3896beba361971802e896d
lfdou <2648142550@qq.com>
2023-01-18 23:52:51 +0800

    JZ30 包含min函数的栈

---
+++ cmd/offer/StackGo.go
@@ -0,0 +1,16 @@
+package main
+package main
+
+func Push(node int) {
+	// write code here
+}
+func Pop() {
+	// write code here
+}
+func Top() int {
+	// write code here
+}
+func Min() int {
+	// write code here
+}
+
