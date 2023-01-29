858a46c6fabf8554e9af843f293d18e92d78a1d4
lfdou <2648142550@qq.com>
2023-01-30 00:02:14 +0800

    push

--- cmd/offer/StackGo.go
+++ cmd/offer/StackGo.go
@@ -1,8 +1,25 @@
 package main
-package main
+
+var stack1 []int
+var stack2 []int
 
 func Push(node int) {
 	// write code here
+
+	stack1 = append(stack1, node)
+
+	if len(stack2) == 0 {
+		stack2 = append(stack2, node)
+		return
+	}
+
+	if node > stack2[len(stack2)-1] {
+		stack2 = append(stack2, stack2[len(stack2)-1])
+	} else {
+		stack2 = append(stack2, node)
+	}
+	return
+
 }
 func Pop() {
 	// write code here
@@ -13,4 +30,3 @@ func Top() int {
 func Min() int {
 	// write code here
 }
-
